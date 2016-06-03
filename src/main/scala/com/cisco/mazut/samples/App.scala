25a79f4ff8948497d71cb400c6d7c213f3bd7457
Dmytro Nezhynskyi <d.nezhinsky@gmail.com>
2016-06-03 15:03:20 +0300

    Initial commit

---
+++ src/main/scala/com/cisco/mazut/samples/App.scala
@@ -0,0 +1,20 @@
+package com.cisco.mazut.samples
+
+import org.apache.spark.SparkContext
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+  def sparkInit(): Unit = {
+  }
+
+}
